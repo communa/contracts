52200dfce26703559939bac9b4bf2e448407243b
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2023-09-14 01:03:46 +0300

    feature: shaping contracts - sending tokens with 5% fee applied - p2

--- contracts/CommunaRouter.sol
+++ contracts/CommunaRouter.sol
@@ -6,27 +6,23 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CommunaRouter {
     address private wallet;
-    address private token = 0x009fd1a3BE53F53beA7D5E67080558BB35843B30;
     uint256 private percentage = 5;
 
     constructor(address _wallet) {
         wallet = _wallet;
     }
 
-    // function transfer(uint _value) public {
-    //     IERC20(token).transferFrom(msg.sender, address(this), _value);
-    //     IERC20(token).transfer(
-    //         address(0x7605c0591Dc69a8C30BaB6b27511183e6617cFE1),
-    //         _value
-    //     );
-    // }
-
-    function transfer(address _to, uint256 _amount) external payable {
-        IERC20(token).transferFrom(msg.sender, address(this), _amount);
+    function transfer(
+        address _token,
+        address _to,
+        uint256 _amount
+    ) external payable {
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
 
         uint256 amountFee = (_amount * percentage) / 100;
         uint256 amountFinal = _amount - amountFee;
 
-        IERC20(token).transfer(_to, amountFinal);
+        IERC20(_token).transfer(wallet, amountFee);
+        IERC20(_token).transfer(_to, amountFinal);
     }
 }
--- scripts/deployRouter.ts
+++ scripts/deployRouter.ts
@@ -1,31 +1,16 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
-const TOKEN_ADDRESS = process.env.TOKEN_ADDRESS as string;
+const COMMUNA_ADDRESS = process.env.COMMUNA_ADDRESS as string;
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
   const CommunaRouter = await ethers.getContractFactory("CommunaRouter");
-  const router = await CommunaRouter.deploy(TOKEN_ADDRESS);
+  const router = await CommunaRouter.deploy(COMMUNA_ADDRESS);
 
   await router.deployed();
 
   console.log("CommunaRouter deployed to:", router.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
